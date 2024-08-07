0e92d7aa6eee190ae5268eb9596d297104619df7
Elmo <elmar.kotte@gmail.com>
2024-08-08 00:16:46 +0200

    Added Global.hpp header file

---
+++ engine/Headers/Global.hpp
@@ -0,0 +1,20 @@
+#ifndef GLOBAL_HPP
+
+#define ULL unsigned long long
+
+#define PAWN 100
+#define KNIGHT 300
+#define BISHOP 320
+#define ROOK 500
+#define QUEEN 900
+#define KING 20000
+
+#include <iostream>
+#include <list>
+#include <map>
+
+enum PieceType{
+ black, p, r,n, b, q, k, white, P, R, N, B, Q, K
+};
+
+#endif
