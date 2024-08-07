0987f7d4934b14c04011eed0e0889611f198b94a
KaiBritt <kaibritt27@gmail.com>
2024-08-07 16:57:13 -0400

    updated board

--- engine/Board.cpp
+++ engine/Board.cpp
@@ -4,10 +4,11 @@
 
 #include "Board.hpp"
 
+
 void print_bit_board(unsigned long long bitboard) {
     for (int row = 0; row < 8; row++) {
         for (int col = 0; col < 8; col++) {
-            if ((bitboard & (1UL << (row*8 + col*8))) != 0) {
+            if ((bitboard & (9223372036854775808UL >> (row*8 + col))) != 0) {
                 printf("1");
             }
             else {
@@ -50,25 +51,24 @@ Board::Board(char * FEN) {
 
     //making black and white bitboardss
     for (int i = 1; i < 7; i++) {
-        bitboards[m['D']] |= m['D'] + i;
-        bitboards[m['W']] |= m['W'] + i;
+        bitboards[m['D']] |= bitboards[m['D'] + i];
+        bitboards[m['W']] |= bitboards[m['W'] + i];
      }
     print_bit_board(bitboards[m['p']]);
 
     //making human readable board #from bb
-    char charMap[15] = "BprnbqkWPRNBQK";
+    char charMap[15] = "DprnbqkWPRNBQK";
     std::map<char,int> charToPoint{{'D',0},{'p',-100},{'r',-500},{'n',-300},{'b',-320},{'q',-900},{'k',-400},{'W',0},{'P',100},{'R',500},{'N',300},{'B',320},{'Q',900},{'K',4000}};
-
+    for (int & val : letterbox) {
+        val = 0;
+    }
     for (int i =0; i < 14; i++) {
         for (int j = 0; j < 64; j++) {
-            if (bitboards[i] & (1<<i) != 0) {
-                letterbox[63 - j] = charToPoint[charMap[i]];
+            unsigned long long val = bitboards[i] & (1UL << (63 - j));
+            if (val != 0UL) {
+                letterbox[j] = charToPoint[charMap[i]];
             }
         }
-    }
-
-    for (float i : letterbox) {
-        printf("%f,",i);
     }
 }
 
