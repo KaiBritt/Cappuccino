2394454f26aa115da2846b06fe2baaf3fef53630
Cinna <jwsmart3@gmail.com>
2024-08-08 00:41:24 +0100

    Adding the adjusted single node eval header file

--- engine/Headers/single_node_evaluation.hpp
+++ engine/Headers/single_node_evaluation.hpp
@@ -4,14 +4,7 @@
 
 #ifndef SINGLE_NODE_EVALUATION_HPP
 #define SINGLE_NODE_EVALUATION_HPP
-#define ULL unsigned long long
-#define PAWN 100
-#define KNIGHT 300
-#define BISHOP 320
-#define ROOK 500
-#define QUEEN 900
-#define KING 20000
-#include <array>
+#include "Global.hpp"
 #include "endgame_piecetables.hpp"
 #include "middlegame_piecetables.hpp"
 
@@ -22,9 +15,9 @@ struct piece_values
     int totalPieceEval;
 };
 
+inline bool endgame = false;
 int flip_square(int index);
-int evaluate_node();
+int evaluate_node(const std::array<int,64>& board);
 piece_values evaluate_piece_values(const std::array<int,64>& board);
-std::array<int,64> adjust_positional_values(const std::array<int,64>& board);
 
 #endif //SINGLE_NODE_EVALUATION_HPP
